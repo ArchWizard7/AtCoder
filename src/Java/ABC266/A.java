c565eb688027d139ccc072842070d47da229a953
ArchWizard7 <ayumu.simoyama.0711@gmail.com>
2022-09-14 19:49:48 +0900

    Solved ABC266-A

--- src/Java/ABC266/A.java
+++ src/Java/ABC266/A.java
@@ -2,7 +2,7 @@ package Java.ABC266;
 
 import java.util.Scanner;
 
-public class Main {
+public class A {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String S = sc.next();
